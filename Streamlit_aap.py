5e912240857787297ef3debaeb08b8f99bb43c29
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-08 04:04:48 +0530

    Update Streamlit_aap.py
    iterative improvement

--- Streamlit_aap.py
+++ Streamlit_aap.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My parents are now healthy')
 streamlit.text('Monkey eats banana')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
