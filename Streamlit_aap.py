70abcf4b8f32396ce99f27651487f6cc99294620
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-14 14:29:29 +0530

    Update Streamlit_aap.py

--- Streamlit_aap.py
+++ Streamlit_aap.py
@@ -3,7 +3,7 @@ import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
 
