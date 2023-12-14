8d66fff2811accf383bb037666f5331a2dceb69f
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-14 15:07:28 +0530

    Update streamlit_aap.py

--- streamlit_aap.py
+++ streamlit_aap.py
@@ -1,8 +1,6 @@
-#streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
 
 import streamlit
 import requests
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
 streamlit.text(fruityvice_response.json())
