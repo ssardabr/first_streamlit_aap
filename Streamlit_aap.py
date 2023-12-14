8fee98c37645db94425a40aefff014efca22bf94
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-14 14:13:33 +0530

    Update Streamlit_aap.py

--- Streamlit_aap.py
+++ Streamlit_aap.py
@@ -1,3 +1,14 @@
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
+
+
+
+---------------------------------------------------------------------------------------------
 import streamlit
 
 streamlit.title('My parents are now healthy')
