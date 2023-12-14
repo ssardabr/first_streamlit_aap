ab2f49ddc12211640e5d0579d333252a400700f1
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-14 19:34:38 +0530

    Update Streamlit_fruit.py

--- Streamlit_fruit.py
+++ Streamlit_fruit.py
@@ -66,3 +66,6 @@ streamlit.dataframe(my_data_rows)
 fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding the jackfruit ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+streamlit.write('Thanks for adding',add_my_fruit )
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
