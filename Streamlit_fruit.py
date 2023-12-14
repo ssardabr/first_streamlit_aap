3512f57792c012cd2b893bd88cab45d666df8485
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-14 19:49:32 +0530

    Update Streamlit_fruit.py

--- Streamlit_fruit.py
+++ Streamlit_fruit.py
@@ -69,3 +69,18 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 
 streamlit.write('Thanks for adding',add_my_fruit )
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:streamlit_error=('Please select a fruit to get information')
+else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit_dataframe(fruitvice_neutralized)
+except URLError as e:
+ streamlit.error()
+
+
+
+
