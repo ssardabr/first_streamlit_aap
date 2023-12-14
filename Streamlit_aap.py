d07be5f7e9337315133cc2f6a2524dcf22bb2826
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-14 14:51:49 +0530

    Update Streamlit_aap.py

--- Streamlit_aap.py
+++ Streamlit_aap.py
@@ -1,16 +1,3 @@
-import streamlit
-
-
-streamlit.header("Fruityvice Fruit Advice!")
-import requests
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-streamlit.text(fruityvice_response.json())
-
-
-
-
 
 import streamlit
 
@@ -39,3 +26,15 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+
+
+
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+import requests
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.text(fruityvice_response.json())
