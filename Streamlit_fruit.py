6081be5f9d9f57b2b4651aa15771cea56f508639
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-14 15:38:13 +0530

    Update Streamlit_fruit.py

--- Streamlit_fruit.py
+++ Streamlit_fruit.py
@@ -37,8 +37,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 #streamlit.dataframe(fruityvice_normalized)
 
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
-streamlit.write('The user entered ', fruit_choice)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
+#streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
