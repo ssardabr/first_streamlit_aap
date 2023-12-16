fd3b0ad6e0a8b460e865ff562e1bc655591832ce
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-16 16:00:39 +0530

    Update Streamlit_fruit.py

--- Streamlit_fruit.py
+++ Streamlit_fruit.py
@@ -104,8 +104,8 @@ try:
       streamlit.dataframe(back_from_function)
 
 
-
 streamlit.header('The fruit load list contains:')
+#streamlit.header('The fruit load list contains:')
 #Snowflake-related functions
 def get_fruit_load_list()
     with my_cnx.cursor() as my_cur:
