259dcb2a48b8fa125fe1c9b7e405e3f131c21727
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-16 18:24:42 +0530

    Update streamlit_end.py

--- streamlit_end.py
+++ streamlit_end.py
@@ -42,3 +42,12 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+#streamlit.text("Hello from Snowflake:")
+streamlit.text("The fruit load list contains:")
+streamlit.text(my_data_row)
