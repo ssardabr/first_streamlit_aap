d7db9aff869225d493000c879d562f0e08d49651
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-16 18:20:24 +0530

    Update streamlit_end.py

--- streamlit_end.py
+++ streamlit_end.py
@@ -28,4 +28,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response)
 
 streamlit.header("Fruityvice Fruit Advice!")
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+streamlit.text(fruityvice_response.json())
