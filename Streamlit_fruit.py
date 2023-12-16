0c1006c3884667ca2d413060b5486e1edbdaf2ea
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-16 17:11:08 +0530

    Update Streamlit_fruit.py

--- Streamlit_fruit.py
+++ Streamlit_fruit.py
@@ -103,8 +103,8 @@ try:
         back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
 
-except URLError as e:
-    streamlit.error()
+#except URLError as e:
+    #streamlit.error()
 
 
   
