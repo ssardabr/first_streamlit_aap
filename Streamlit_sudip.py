368b11bab7c801156dbf2804ce8a247c89e3da00
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-16 22:33:43 +0530

    Update Streamlit_sudip.py

--- Streamlit_sudip.py
+++ Streamlit_sudip.py
@@ -98,6 +98,12 @@ def get_fruityvice_data(this_fruit_choice):
 #except URLError as e:
     #streamlit.error()
 
+streamlit.header("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list()
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
 
 
 
