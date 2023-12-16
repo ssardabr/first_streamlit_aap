c3a14eea772d53d8018552c56026bc2f45e5c8c6
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-16 15:52:30 +0530

    Update Streamlit_fruit.py

--- Streamlit_fruit.py
+++ Streamlit_fruit.py
@@ -113,6 +113,12 @@ try:
     #   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
      #  streamlit.dataframe(fruitvice_normalized)'''
 
+streamlit.header("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list()
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
 
 
 
