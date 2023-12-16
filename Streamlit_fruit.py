1e4c299d45a2f868c1755e8dc76ca87b0d01f4a2
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-16 18:31:16 +0530

    Update Streamlit_fruit.py

--- Streamlit_fruit.py
+++ Streamlit_fruit.py
@@ -73,49 +73,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 
 #streamlit.write('Thanks for adding',add_my_fruit )
 #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
 #from urllib.error import URLError
 
-streamlit.header('Fruityvice Fruit Advice!')
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-       streamlit.error=("Please select a fruit to get information.")
-  else:
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-       streamlit.dataframe(fruitvice_normalized)
 
-except URLError as e:
-    streamlit.error()
-
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
-    
-streamlit.header('Fruityvice Fruit Advice!')
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-       streamlit.error("Please select a fruit to get information.")
-  else:
-        back_from_function = get_fruityvice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
-
-except URLError as e:
-    streamlit.error()
-
-'''streamlit.header('The fruit load list contains:')
-
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("select * from fruit_load_list")
-         return my_cur.fetchall()
-if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.cnnect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    streamlit.dataframe(my_data_rows)'''
 
 
 
