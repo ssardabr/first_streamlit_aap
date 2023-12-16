621640f7277b1a4fc3d7102ccd71eb677b51c6b1
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-16 15:58:57 +0530

    Update Streamlit_fruit.py

--- Streamlit_fruit.py
+++ Streamlit_fruit.py
@@ -103,17 +103,9 @@ try:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
 
-#streamlit.header('Fruityvice Fruit Advice!')
-#try:
- # fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  #if not fruit_choice:
-   #    streamlit.error=("Please select a fruit to get information.")
-  #else:
-   #    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    #   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-     #  streamlit.dataframe(fruitvice_normalized)'''
 
-streamlit.header("The fruit load list contains:")
+
+streamlit.header('The fruit load list contains:')
 #Snowflake-related functions
 def get_fruit_load_list()
     with my_cnx.cursor() as my_cur:
