5e024bea71901defc028176201571b6f5fdc2496
ssardabr <152769406+ssardabr@users.noreply.github.com>
2023-12-16 18:23:40 +0530

    Update streamlit_end.py

--- streamlit_end.py
+++ streamlit_end.py
@@ -40,3 +40,5 @@ streamlit.text(fruityvice_response.json())
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Apple')
 streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
